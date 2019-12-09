c14b2b38202ce16eda88cbaa6e7cdd11c0b000cf
poorva <poorva.ramani@gmail.com>
2019-12-08 19:46:31 -0600

    fixing UI

--- assets/js/apiRoutes.js
+++ assets/js/apiRoutes.js
@@ -8,47 +8,36 @@ var dbData = require("../db/db.json");
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
   // API GET Requests
-  app.get("/api/notes", function(req, res) {
-    res.json(dbData);  
+  app.get("/api/notes", function (req, res) {
+    res.json(dbData);
   });
 
   // API POST Requests
-  app.post("/api/notes", function(req, res) {
-      const newNote = {
-       id: dbData.length+1,
-       title: req.body.title,
-       text: req.body.text
-      };
-      dbData.push(newNote);
-      res.json(dbData);
-      console.log(dbData)
+  app.post("/api/notes", function (req, res) {
+    const newNote = {
+      id: dbData.length + 1,
+      title: req.body.title,
+      text: req.body.text
+    };
+    dbData.push(newNote);
+    res.json(dbData);
+    console.log(dbData)
   });
 
-  app.delete("/api/notes/:id", function(req, res) {
+  // API DELETE Requests
+  app.delete("/api/notes/:id", function (req, res) {
     const reqId = req.params.id;
     let note = dbData.filter(note => {
       return note.id == reqId;
     })[0];
     const index = dbData.indexOf(note);
-    dbData.splice(index,1);
-    for(var i=index;i<dbData.length;i++){
+    dbData.splice(index, 1);
+    for (var i = index; i < dbData.length; i++) {
       dbData[i].id--;
-    }  
-   console.log(dbData)
-res.json(dbData);
- });
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-//   app.post("/api/clear", function(req, res) {
-//     // Empty out the arrays of data
-//     tableData.length = 0;
-//     waitListData.length = 0;
-
-//     res.json({ ok: true });
-//   });
- }
+    }
+    console.log(dbData)
+    res.json(dbData);
+  });
+}
