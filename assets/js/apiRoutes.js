1ac36f15288516f6f06679abad78fd0e2be27a55
poorva <poorva.ramani@gmail.com>
2019-12-08 19:28:02 -0600

    adding logic to delete notes

--- assets/js/apiRoutes.js
+++ assets/js/apiRoutes.js
@@ -10,21 +10,35 @@ var dbData = require("../db/db.json");
 
 module.exports = function(app) {
   // API GET Requests
-
   app.get("/api/notes", function(req, res) {
-    res.json(dbData);
+    res.json(dbData);  
   });
 
   // API POST Requests
   app.post("/api/notes", function(req, res) {
-      dbData.push(req.body);
-      res.json(true);
+      const newNote = {
+       id: dbData.length+1,
+       title: req.body.title,
+       text: req.body.text
+      };
+      dbData.push(newNote);
+      res.json(dbData);
+      console.log(dbData)
   });
 
-//   app.delete("/api/notes/:id", function(req, res) {
-//     dbData.push(req.body);
-//     res.json(true);
-// });
+  app.delete("/api/notes/:id", function(req, res) {
+    const reqId = req.params.id;
+    let note = dbData.filter(note => {
+      return note.id == reqId;
+    })[0];
+    const index = dbData.indexOf(note);
+    dbData.splice(index,1);
+    for(var i=index;i<dbData.length;i++){
+      dbData[i].id--;
+    }  
+   console.log(dbData)
+res.json(dbData);
+ });
 
   // ---------------------------------------------------------------------------
   // I added this below code so you could clear out the table while working with the functionality.
@@ -37,4 +51,4 @@ module.exports = function(app) {
 
 //     res.json({ ok: true });
 //   });
- };
+ }
